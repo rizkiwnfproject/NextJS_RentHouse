469e95119ae66012d9821416f1a926bf78931254
rizkiwnfproject <rizkiwahyuproject@gmail.com>
2025-10-16 14:34:52 +0700

    integration all listing home

---
+++ src/services/listing.service.ts
@@ -0,0 +1,15 @@
+import { apiSlice } from "./base-query";
+
+export const listingApi = apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+        getAllListing: builder.query({
+            query: () =>({
+                url: '/listing',
+                method: "GET"
+            })
+        })
+    })
+})
+
+
+export const {useGetAllListingQuery} = listingApi
